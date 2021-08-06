4e8a0cbc6eecce6fc794a564355a4db2dabf9001
Mikhail <nemtinovmikhail@gmail.com>
2021-08-06 17:54:17 +0300

    Добавлен учебный калькулятор

---
+++ Learn/Complex.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Learn
+{
+    class Complex
+    {
+        public double _re;
+        public double _im;
+
+        public Complex(double im, double re)
+        {
+            _re = re;
+            _im = im;
+        }
+
+        public static Complex Substruct(Complex a, Complex b)
+        {
+            Complex sub = new Complex(0, 0);
+            sub._re = a._re - b._re;
+            sub._im = a._im - b._im;
+            return sub;
+        }
+
+        public override string ToString()
+        {
+            string output;
+            if (_im != 0 && _im >= 0 && _im != 1 && _im != -1)
+                output = $"{_re} + {_im}i";
+            else if (_im != 0 && _im != 1 && _im != -1)
+                output = $"{_re} - {-_im}i";
+            else if (_im != 0 && _im != 1)
+                output = $"{_re} - i";
+            else if (_im != 0)
+                output = $"{_re} + i";
+            else
+                output = $"{_re}";
+            return output;
+        }
+    }
+}
---
+++ SimpleCalc/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace SimpleCalc
+{
+    class Program
+    {
+        static void Main()
+        {
+            double num1 = 0;
+            double num2 = 0;
+            char operand;
+            bool errStatus;
+            Console.WriteLine("Введите число 1");
+            errStatus = double.TryParse(Console.ReadLine(), out num1);
+            error(errStatus, "ввода данных числа 1");
+            Console.WriteLine("Введите операнд");
+            errStatus = char.TryParse(Console.ReadLine(), out operand);
+            error(errStatus, "ввода данных операнда");
+            Console.WriteLine("Введите число 2");
+            errStatus = double.TryParse(Console.ReadLine(), out num2);
+            error(errStatus, "ввода данных числа 2");
+            Console.WriteLine("Результат: ");
+            Console.WriteLine(Operation(num1, num2, operand));
+            Console.ReadKey();
+        }
+
+        static double Operation(double num1, double num2, char operand)
+        {
+            double result = 0;
+            switch (operand)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+                case ':':
+                    result = num1 / num2;
+                    break;
+                case '^':
+                    result = num1;
+                    for(int i = 1; i < num2; i++)
+                    result *= num1;
+                    break;
+                case '%':
+                    result = (num1 / 100) * num2;
+                    break;
+                default:
+                    error(false, ", несуществующий операнд");
+                    break;
+            }
+            return result;
+        }
+
+        static void error(bool status, string name)
+        {
+            if (status == false)
+                Console.WriteLine($"Ошибка {name}"); 
+        }
+    }
+}

e003a9869b2dacdbc975203f07813f13f32433fa
Mikhail <nemtinovmikhail@gmail.com>
2021-08-08 18:05:16 +0300

    Алгоритм поиска дубликатов(нерабочий)

---
+++ DuplicationCheck(Tortoise and hare)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DuplicationCheck_Tortoise_and_hare_
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] nums = new int[10] {1, 5, 4, 7, 6, 3, 9, 4, 0, 8 };
+            int tortoise = 0;
+            int hare = 0;
+            Console.WriteLine("1, 5, 4, 7, 6, 3, 9, 4, 0, 8");
+            while (true)
+            {
+                tortoise = nums[tortoise];
+                hare = nums[nums[hare]];
+                if (tortoise == hare)
+                    break;
+            }
+            int ptr1 = nums[0];
+            int ptr2 = tortoise;
+            while (ptr1 != ptr2)
+            {
+                ptr1 = nums[ptr1];
+                ptr2 = nums[ptr2];
+            }
+            Console.WriteLine(tortoise + " " + hare);
+            Console.WriteLine(ptr1);
+            Console.ReadKey();
+        }
+    }
+}
